2db5c04aaa62d745981f9d39887d0a653b54de77
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2022-11-20 21:47:55 +0700

    change to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,23 @@
 import Card from "../components/Card";
-import useFetch from "../services/useFetchPosts";
+import { API_URL } from "../services/constants";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-  const [data] = useFetch("posts", "1");
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    fetch(`${API_URL}posts?_start=1&_limit=10`).then((res) =>
+      res.json().then((data) => setPosts(data))
+    );
+  }, []);
 
   return (
     <main className="w-full">
       <div className="container mx-auto py-10">
-        <div className="w-full grid grid-cols-3 gap-y-3 gap-x-3">
-          {data &&
-            data.map((item) => {
-              return <Card key={item.id} article={item} />;
+        <div className="w-full grid grid-cols-3 gap-y-3 gap-x-3 mb-5">
+          {posts &&
+            posts.map((post) => {
+              return <Card key={post.id} article={post} />;
             })}
         </div>
       </div>
